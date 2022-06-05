61e4f92297300ebf72694a4081e4a1ca3946e494
Alvin <alvin.tol@hotmail.com>
2022-06-05 12:58:56 -0600

    adds accountReducer logic

--- src/state/reducers/acountReducer.tsx
+++ src/state/reducers/acountReducer.tsx
@@ -1,1 +1,17 @@
-export {};
+interface Action {
+  type: string;
+  payload: number;
+}
+
+const reducer = (state: number = 0, action: Action) => {
+  switch (action.type) {
+    case 'deposit':
+      return state + action.payload;
+    case 'withdraw':
+      return state - action.payload;
+    default:
+      return state;
+  }
+};
+
+export default reducer;
